654179826e989bf415781a068cd9e68aff61f36a
D1scoDancer <apshulikov@edu.hse.ru>
2024-11-13 01:05:00 +0300

    initial commit

---
+++ src/test/java/alekssey7227/mygpt/MyGptApplicationTests.java
@@ -0,0 +1,13 @@
+package alekssey7227.mygpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
